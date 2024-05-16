e93df405b7e8e3dd41924731e0d2bb5415a18526
NarcissusNull <NarcissusNull@outlook.com>
2024-05-16 11:16:21 +0800

    添加默认数据

--- src/service/management-expense.service.ts
+++ src/service/management-expense.service.ts
@@ -25,6 +25,7 @@ export class ManagementExpenseService {
       { projectName: '项目2', costItem: '其他', month: '2023-04', paymentTime: new Date('2023-04-15'), payee: '李四', paymentMethod: '微信红包', transferNote: '备注2', reason: '事由2' },
       { projectName: '项目3', costItem: '公关费', month: '2023-03', paymentTime: new Date('2023-03-10'), payee: '王五', paymentMethod: '支付宝', transferNote: '备注3', reason: '事由3' },
       { projectName: '项目4', costItem: '其他', month: '2023-02', paymentTime: new Date('2023-02-20'), payee: '赵六', paymentMethod: '招商', transferNote: '备注4', reason: '事由4' },
+      { projectName: '项目5', costItem: '公关费', month: '2023-01', paymentTime: new Date('2023-01-15'), payee: '孙七', paymentMethod: '微信红包', transferNote: '备注5', reason: '事由5' },
       { projectName: '项目5', costItem: '公关费', month: '2023-01', paymentTime: new Date('2023-01-15'), payee: '孙七', paymentMethod: '微信红包', transferNote: '备注5', reason: '事由5' }
     ];
   }
