ab5cc96e8338997b30ef190d888aab45d4ccf52c
NarcissusNull <NarcissusNull@outlook.com>
2024-05-16 11:12:50 +0800

    test

--- src/service/income.service.ts
+++ src/service/income.service.ts
@@ -28,7 +28,7 @@ export class IncomeService {
       { projectName: '项目5', signUnit: '单位E', costItem: '服务费', month: '2023-01', invoiceAmount: 15000, receivedFee: 12000, paymentTime: new Date('2023-01-15'), contractDuration: '2023-2025', withdrawTime: new Date('2023-01-01'), status: '在施', taxRate: 0.07, taxNote: '无' },
       { projectName: '项目6', signUnit: '单位F', costItem: '个人借款', month: '2022-12', invoiceAmount: 4000, receivedFee: 3500, paymentTime: new Date('2022-12-20'), contractDuration: '2023-2025', withdrawTime: new Date('2023-03-01'), status: '结束', taxRate: 0.05, taxNote: '无' },
       { projectName: '项目7', signUnit: '单位G', costItem: '转存', month: '2022-11', invoiceAmount: 6000, receivedFee: 5500, paymentTime: new Date('2022-11-10'), contractDuration: '2023-2024', withdrawTime: new Date('2023-04-01'), status: '在施', taxRate: 0.06, taxNote: '无' },
-      { projectName: '项目8', signUnit: '单位G', costItem: '转存', month: '2022-111', invoiceAmount: 6000, receivedFee: 5500, paymentTime: new Date('2022-11-10'), contractDuration: '2023-2024', withdrawTime: new Date('2023-04-01'), status: '在施', taxRate: 0.06, taxNote: '无' },
+      { projectName: '项目81', signUnit: '单位G', costItem: '转存', month: '2022-111', invoiceAmount: 6000, receivedFee: 5500, paymentTime: new Date('2022-11-10'), contractDuration: '2023-2024', withdrawTime: new Date('2023-04-01'), status: '在施', taxRate: 0.06, taxNote: '无' },
       { projectName: '项目91', signUnit: '单位G', costItem: '转存', month: '2022-111', invoiceAmount: 6000, receivedFee: 5500, paymentTime: new Date('2022-11-10'), contractDuration: '2023-2024', withdrawTime: new Date('2023-04-01'), status: '在施', taxRate: 0.06, taxNote: '无' }
     ];
   }
